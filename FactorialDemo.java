a2add56ff145666c444d5b7cac5762b6b4ce3fa4
jy1dyz <jyldyz.kgz@gmail.com>
2019-06-19 23:10:27 +0600

    first commit factorial

---
+++ FactorialDemo.java
@@ -0,0 +1,30 @@
+//пример статических методов
+class Factorial {
+    //вычисление факториала в цикле
+    static long computeI(int n) {
+        long result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    //вычисление факториала рекурсивным способом
+    static long computeR(int n) {
+        if (n == 1) return n;
+        return computeR(n-1)*n;
+    }
+}
+
+public class FactorialDemo {
+    public static void main(String[] args) {
+        Factorial f = new Factorial();
+        System.out.println("Вычисление факториала в цикле: ");
+        for (int i = 6; i > 0; i--) {
+            System.out.println(i + "!= "+Factorial.computeI(i));
+        }
+        System.out.println("ВЫчисление факториала рекурсивным методом:");
+        for (int r= 1; r <= 6; r++) {
+            System.out.println(r + "!= "+Factorial.computeR(r));
+        }
+    }
+}
